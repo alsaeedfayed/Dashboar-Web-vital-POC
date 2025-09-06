5407835c3ea7fda37fc046acfa1285c0d5af3ec3
alsaeedfayed <alsaeedfayed@gmail.com>
2025-09-06 00:46:37 -0400

    feat: :fire: installing web-vitals lib & chart.js & provide client hydreation withIncrementalHydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+  withIncrementalHydration,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideClientHydration(withIncrementalHydration()),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+  ],
 };
